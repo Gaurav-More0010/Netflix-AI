81951b4305e0879693f0dcd19bf7a95fc1fa8731
Gaurav More <gauravmore0010@gmail.com>
2023-08-27 17:16:34 +0530

    "2nd Test Commit"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Heeloooo</h1>
+          <h1>Heelo</h1>
         </p>
         <a
           className="App-link"
