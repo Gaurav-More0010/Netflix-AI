154b4e1821dd891b9428b7745df1c4c46d22840c
Gaurav More <gauravmore0010@gmail.com>
2023-08-30 17:22:54 +0530

    "API Integration, Custom Hooks"

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,13 @@ export const LOGO =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const USER_AVATAR =
   "https://media.licdn.com/dms/image/C4D03AQHdRZ09tLnlXw/profile-displayphoto-shrink_800_800/0/1656833201135?e=1698883200&v=beta&t=rc832BAhbXMv0Z1DtXtxyhgj_G7dkeTMN5x5-h2hFd4";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjk3MmNjZmVmM2YwZDliYjI0NzNkZGRkZDk0ZmI0YiIsInN1YiI6IjY0ZWYwNDMxZTBjYTdmMDBhZTM4NjVhOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ryHbdSMU9VcotGWtZbmiS9kv5l5KO7yj8vSN-YX2zV0",
+    //url: "https://api.themoviedb.org/3/movie/now_playing?page=1",
+  },
+};
