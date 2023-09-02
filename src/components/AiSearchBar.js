d27e1602ee695cb1aaad36751477bb836427ebfe
Gaurav More <gauravmore0010@gmail.com>
2023-09-02 13:20:35 +0530

    "Design Searchbar"

--- src/components/AiSearchBar.js
+++ src/components/AiSearchBar.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const AiSearchBar = () => {
   return (
-    <div className="pt-[20%]">
-      <form className=" w-1/2 m-6 bg-black">
+    <div className="pt-[10%] flex flex justify-center">
+      <form className=" w-1/2 bg-black grid grid-cols-12">
         <input
           type="text"
-          className="p-4 m-4"
+          className="p-4 m-4 col-span-9"
           placeholder="What would you like to watch today?"
         />
-        <button className="bg-red-700 rounded-lg text-white py-2 px-4">
+        <button className="m-4 col-span-3 bg-red-700 rounded-lg text-white py-2 px-4">
           Search
         </button>
       </form>
